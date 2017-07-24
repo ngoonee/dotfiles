6b5ceeb7be3333aa8ad3305e12ebee05487f3673
Ng Oon-Ee <ngoonee@gmail.com>
2017-07-24 12:18:04 +0800

    Add sql layer

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     sql
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
@@ -57,6 +58,7 @@ values."
      beancount
      themes-megapack
      spell-checking
+     sql
      syntax-checking
      version-control
      xkcd
