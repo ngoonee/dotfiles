5fd81a9d9449111acad92b327ecf7d33fb0a7b71
Ng Oon-Ee <ngoonee@gmail.com>
2017-11-08 10:54:05 +0800

    Add some themes to spacemacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -149,7 +149,9 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(wheatgrass
+   dotspacemacs-themes '(ample
+                         afternoon
+                         wheatgrass
                          grandshell
                          moe-dark
                          ujelly
