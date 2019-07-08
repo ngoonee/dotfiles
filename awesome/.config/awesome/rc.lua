8210e8eeb0eec54f1dacf10823f66ac415b18aa5
Ng Oon-Ee <ngoonee@gmail.com>
2019-07-08 15:53:10 +0800

    Fix layouts

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -375,11 +375,11 @@ globalkeys = awful.util.table.join(
 
 
     -- Widgets popups
-    awful.key({ altkey, }, "c", function () if beautiful.cal then beautiful.cal.show(7) end end,
+    awful.key({ altkey, modkey }, "c", function () if beautiful.cal then beautiful.cal.show(7) end end,
               {description = "show calendar", group = "widgets"}),
-    awful.key({ altkey, }, "h", function () if beautiful.fs then beautiful.fs.show(7) end end,
+    awful.key({ altkey, modkey }, "h", function () if beautiful.fs then beautiful.fs.show(7) end end,
               {description = "show filesystem", group = "widgets"}),
-    awful.key({ altkey, }, "w", function () if beautiful.weather then beautiful.weather.show(7) end end,
+    awful.key({ altkey, modkey }, "w", function () if beautiful.weather then beautiful.weather.show(7) end end,
               {description = "show weather", group = "widgets"}),
 
     -- Prompt
--- awesome/.config/awesome/themes/powerarrow-dark-custom/theme.lua
+++ awesome/.config/awesome/themes/powerarrow-dark-custom/theme.lua
@@ -275,7 +275,7 @@ function theme.at_screen_connect(s)
     gears.wallpaper.maximized(wallpaper, s, true)
 
     -- Tags
-    awful.tag(awful.util.tagnames, s, awful.layout.layouts)
+    awful.tag(awful.util.tagnames, s, awful.layout.suit.tile)
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
