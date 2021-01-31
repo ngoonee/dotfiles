3a49c51aa650f47d1ac764b4cd935fd346c530c2
Ng Oon-Ee <ngoonee@gmail.com>
2021-01-31 10:48:16 +0800

    i3 updates for handling monitors awesomewm style

--- i3/.config/i3/i3-workspace.sh
+++ i3/.config/i3/i3-workspace.sh
@@ -3,9 +3,25 @@
 action="$1"
 workspace="$2"
 
-focused_workspace=$(i3-msg -t get_workspaces | jq --raw-output '.[]|select(.focused).name')
+#focused_workspace=$(i3-msg -t get_workspaces | jq --raw-output '.[]|select(.focused).name')
 
-active_postfix=${focused_workspace: -1}
+#active_postfix=${focused_workspace: -1}
+
+focused_output=$(i3-msg -t get_workspaces | jq --raw-output '.[]|select(.focused).output')
+
+if  [[ "$focused_output" == "eDP1" ]];
+then
+    active_postfix=a
+elif  [[ "$focused_output" == "DP1" ]];
+then
+    active_postfix=b
+elif  [[ "$focused_output" == "DP2" ]];
+then
+    active_postfix=c
+elif  [[ "$focused_output" == "VIRTUAL1" ]];
+then
+    active_postfix=d
+fi
 
 target_workspace=${workspace}${active_postfix}
 echo $active_postfix
