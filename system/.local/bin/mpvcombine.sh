bcbf9db16ba16895ed097656f46118d48285bff5
Ng Oon-Ee <ngoonee@gmail.com>
2021-07-27 17:30:07 +0800

    Update MPVcombine script

--- system/.local/bin/mpvcombine.sh
+++ system/.local/bin/mpvcombine.sh
@@ -2,7 +2,7 @@
 
 for f in ./*.mkv;
 do
-  if [ -e "${f/.pdf}.png" ] ; then
+  if [ -e "${f/.mkv}.mp4" ] ; then
     echo "Already processed"
   else
     echo "Processing $f"
@@ -10,10 +10,10 @@ do
   fi
 done
 
-for f in 2021-06-14*.mp4; do
+for f in 2021-07-26*.mp4; do
     echo "file '$f'" >> concatlist.txt
 done
 
-ffmpeg -f concat -safe 0 -i concatlist.txt -c copy 2021-06-14-ArtificialIntelligenceFullLecture.mp4
+ffmpeg -f concat -safe 0 -i concatlist.txt -c copy 2021-07-26-ArtificialIntelligenceFullLecture.mp4
 
 rm concatlist.txt
