7ec3a5242c7b353577cd7dacaed59dc00875ff22
Ng Oon-Ee <ngoonee@gmail.com>
2021-09-02 09:04:12 +0800

    Custom font per system for spacemacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -277,8 +277,8 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Inconsolata"
-                               :size 14
+   dotspacemacs-default-font `("Inconsolata"
+                               :size ,(if (string-equal "ngdesktop" (system-name)) 18 14)
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
